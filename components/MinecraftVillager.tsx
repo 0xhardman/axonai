92357df5d50676fbbb4c83688400ae6348c7cc05
0xhardman <1142783160@qq.com>
2025-02-03 16:59:05 +0800

    change positon

--- components/MinecraftVillager.tsx
+++ components/MinecraftVillager.tsx
@@ -1,42 +1,17 @@
 import * as THREE from "three";
 import { useGLTF } from "@react-three/drei";
-import { useRef, useEffect } from "react";
+import { useRef } from "react";
 
 export function MinecraftVillager() {
   const gltf = useGLTF("/assets/minecraft_villager/scene.gltf");
   const groupRef = useRef<THREE.Group>(null!);
 
-  useEffect(() => {
-    gltf.scene.traverse((child) => {
-      if (child instanceof THREE.Mesh) {
-        if (child.material) {
-          // 如果是单个材质
-          if (child.material instanceof THREE.Material) {
-            child.material.transparent = false;
-            child.material.opacity = 1;
-            child.material.depthWrite = true;
-          }
-          // 如果是材质数组
-          else if (Array.isArray(child.material)) {
-            child.material.forEach(mat => {
-              if (mat) {
-                mat.transparent = false;
-                mat.opacity = 1;
-                mat.depthWrite = true;
-              }
-            });
-          }
-        }
-      }
-    });
-  }, [gltf]);
-
   return (
     <group
       ref={groupRef}
-      position={[0, 3, 0]}
-      rotation={[0, Math.PI, 0]}
-      scale={0.25}
+      position={[0, 3, 0]}     // [x, y, z] 位置
+      rotation={[0, Math.PI, 0]}  // [x, y, z] 旋转，使用弧度
+      scale={0.3}             // 整体缩放
     >
       <primitive object={gltf.scene} />
     </group>
--- components/Scene.tsx
+++ components/Scene.tsx
@@ -26,59 +26,62 @@ export function Scene() {
   const controlsRef = useRef();
 
   return (
-    <div className="fixed left-0 top-1/2 -translate-y-1/2">
-      <div className="w-[500px] h-[500px] border rounded-full">
-        <Canvas style={{ width: "500px", height: "500px" }}>
-          <CameraSetup />
-          <OrbitControls
-            ref={controlsRef}
+    <div className="flex">
+      <div className="relative w-[500px] h-[500px]">
+        <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-[300px] h-[300px] border rounded-full bg-gray-400"></div>
+        <div className="w-[500px] h-[500px]">
+          <Canvas style={{ width: "500px", height: "500px" }}>
+            <CameraSetup />
+            <OrbitControls
+              ref={controlsRef}
 
-          // enableZoom={true}
-          // enablePan={true}
-          // enableRotate={true}
-          // target={[0, 0, 0]}
-          // onChange={(e) => {
-          //   const camera = e.target.object;
-          //   setCameraInfo({
-          //     position: [
-          //       Number(camera.position.x.toFixed(2)),
-          //       Number(camera.position.y.toFixed(2)),
-          //       Number(camera.position.z.toFixed(2))
-          //     ],
-          //     rotation: [
-          //       Number(camera.rotation.x.toFixed(2)),
-          //       Number(camera.rotation.y.toFixed(2)),
-          //       Number(camera.rotation.z.toFixed(2))
-          //     ]
-          //   });
-          // }}
-          />
-          <ambientLight intensity={0.7} />
-          <directionalLight
-            intensity={2}
-            position={[3, 3, 3]}
-            castShadow
-          />
-          <directionalLight
-            intensity={1.5}
-            position={[-3, -3, -3]}
-          />
-          <MinecraftVillager />
-          <ContactShadows
-            opacity={0.8}
-            scale={10}
-            blur={2}
-            far={10}
-            resolution={256}
-            color="#000000"
-          />
-          <Stats />
-        </Canvas>
+            // enableZoom={true}
+            // enablePan={true}
+            // enableRotate={true}
+            // target={[0, 0, 0]}
+            // onChange={(e) => {
+            //   const camera = e.target.object;
+            //   setCameraInfo({
+            //     position: [
+            //       Number(camera.position.x.toFixed(2)),
+            //       Number(camera.position.y.toFixed(2)),
+            //       Number(camera.position.z.toFixed(2))
+            //     ],
+            //     rotation: [
+            //       Number(camera.rotation.x.toFixed(2)),
+            //       Number(camera.rotation.y.toFixed(2)),
+            //       Number(camera.rotation.z.toFixed(2))
+            //     ]
+            //   });
+            // }}
+            />
+            <ambientLight intensity={0.7} />
+            <directionalLight
+              intensity={2}
+              position={[3, 3, 3]}
+              castShadow
+            />
+            <directionalLight
+              intensity={1.5}
+              position={[-3, -3, -3]}
+            />
+            <MinecraftVillager />
+            <ContactShadows
+              opacity={0.8}
+              scale={10}
+              blur={2}
+              far={10}
+              resolution={256}
+              color="#000000"
+            />
+            {/* <Stats /> */}
+          </Canvas>
+        </div>
       </div>
-      <div className="absolute top-0 left-0 bg-black/50 text-white p-2 rounded m-2 font-mono text-xs">
+      {/* <div className="absolute top-0 left-0 bg-black/50 text-white p-2 rounded m-2 font-mono text-xs">
         <div>Position: [{cameraInfo.position.join(', ')}]</div>
         <div>Rotation: [{cameraInfo.rotation.join(', ')}]</div>
-      </div>
+      </div> */}
     </div>
   );
 }
